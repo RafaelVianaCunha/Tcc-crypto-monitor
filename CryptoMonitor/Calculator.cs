0df74f9e195b50dd48c3613d176794aa795b9a47
Luis Gomes <luis.csg96@gmail.com>
2020-04-16 00:04:30 -0300

    Init project

---
+++ CryptoMonitor/Calculator.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoMonitor {
+    public class Calculator {
+        public IDictionary<string, decimal> values { get; }
+        public decimal StopLimit { get; }
+
+        public Calculator(decimal stopLimit) 
+        {
+            StopLimit = stopLimit;
+            values = new ConcurrentDictionary<string, decimal>();
+        }
+
+        public void CalculateForNewValue(decimal newValue, string exchange) 
+        {
+            Console.WriteLine($"Exchange {exchange} Valor: {newValue}");
+
+            if (values.TryGetValue(exchange, out _))
+            {
+                values[exchange] = newValue;
+            } 
+            else 
+            {
+                values.Add(exchange, newValue);
+            }
+
+            var median = GetMedian(values.Select(v => v.Value).ToList());
+
+            Console.WriteLine("Mediana " + median);
+
+            if (StopLimit >= median) {
+                Console.WriteLine("Tem que vender");
+            }
+        }
+
+        public decimal GetMedian(IList<decimal> numbers)
+        {
+            int numberCount = numbers.Count();
+            int halfIndex = numbers.Count() / 2;
+
+            var sortedNumbers = numbers.OrderBy(n => n);
+
+            decimal median;
+
+            if ((numberCount % 2) == 0)
+            {
+                var num1 = sortedNumbers.ElementAt(halfIndex);
+                var num2 = sortedNumbers.ElementAt(halfIndex - 1);
+
+                median = (num1 + num2) / 2;
+            } else {
+                median = sortedNumbers.ElementAt(halfIndex);
+            }
+
+            return median;
+        }
+    }
+}
---
+++ CryptoMonitor/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bitfinex.Client.Websocket;
+using Bitfinex.Client.Websocket.Client;
+using Bitfinex.Client.Websocket.Requests.Subscriptions;
+using Bitfinex.Client.Websocket.Websockets;
+using Websocket.Client;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CryptoMonitor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var exitEvent = new ManualResetEvent(false);
+            // var url = BitfinexValues.ApiWebsocketUrl;
+
+            // using (var communicator = new BitfinexWebsocketCommunicator(url))
+            // {
+            //     using (var client = new BitfinexWebsocketClient(communicator))
+            //     {
+            //         client.Streams.InfoStream.Subscribe(info =>
+            //         {
+            //             Console.WriteLine($"Info received, reconnection happened, resubscribing to streams");
+                        
+            //             client.Send(new TickerSubscribeRequest("BTC/USD"));
+            //         });
+
+            //         client.Streams.TickerStream.Subscribe(ticker => 
+            //         {
+            //             Console.WriteLine($"{DateTime.Now} Compra: {ticker.Bid} Venda {ticker.Ask}");
+            //         });
+
+            //         await communicator.Start();
+
+            //         exitEvent.WaitOne();
+            //     }
+            // }
+
+            // var binanceUrl = new Uri("wss://dex.binance.org/api/ws");
+            // using (var ws = new WebsocketClient(binanceUrl)) {
+            //     await ws.Start();
+                
+            //     var message = JsonConvert.SerializeObject(new {
+            //         method = "subscribe",
+            //         topic = "allTickers",
+            //         symbols = new string[] { "$all" }
+            //     });
+
+            //     Console.WriteLine(message);
+
+            //     ws.Send(message);
+
+            //     ws.MessageReceived.Subscribe(msg => Console.WriteLine(msg));
+
+            //     exitEvent.WaitOne();
+            // };
+
+            // Console.ReadKey();
+            // Console.WriteLine("Hello World!");
+
+            var calculator = new Calculator(97.0M);
+
+            var task1 = Task.Run(() => {
+                for (var i = 0; i < 100; i++)
+                {
+                    var x = new Random().Next(95, 101);
+                    calculator.CalculateForNewValue(x, "Binance");
+                }
+            });
+
+            var task2 = Task.Run(() => {
+                for (var i = 0; i < 100; i++)
+                {
+                    var x = new Random().Next(95, 101);
+                    calculator.CalculateForNewValue(x, "Bitfinex");
+                }
+            });
+
+            var task3 = Task.Run(() => {
+                for (var i = 0; i < 100; i++)
+                {
+                    var x = new Random().Next(95, 101);
+                    calculator.CalculateForNewValue(x, "Bybit");
+                }
+            });
+
+            await Task.WhenAll(task1, task2, task3);
+        }
+    }
+}
